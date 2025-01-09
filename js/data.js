74ca621ecff83393935b314ad5939018dc135f96
Felopateernabil27 <Felofelo495@gmail.com>
2025-01-09 13:17:56 -0800

    Now

---
+++ js/data.js
@@ -0,0 +1,30 @@
+let city=[
+    {
+        img:"lisbon-3.jpg",
+        name:"Lisbon",
+        happy:"1600+ happy eaters",
+        stars:"60+ top chefs",
+        twitter:"@omnifood_lx"
+    },
+    {
+        img:"san-francisco.jpg",
+        name:"San Francisco",
+        happy:"3700+ happy eaters",
+        stars:"160+ top chefs",
+        twitter:"@omnifood_sf"
+    },
+    {
+        img:"berlin.jpg",
+        name:"Berlin",
+        happy:"2300+ happy eaters",
+        stars:"110+ top chefs",
+        twitter:"@omnifood_berlin"
+    },
+    {
+        img:"london.jpg",
+        name:"London",
+        happy:"1200+ happy eaters",
+        stars:"50+ top chefs",
+        twitter:"@omnifood_london"
+    },
+]
